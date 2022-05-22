6eb5db1a420a6591da2af0aa6cdf0bef2902bc49
thakurSaad <thakursaad613@gmail.com>
2022-05-22 13:53:30 +0600

    installed firebase, react firebase hooks, tailwind, daisyUI, react router v6 and deployed on firebase

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Alpha Steelwork
+
+### Technologies
+- react js
+- firebase
+- react firebase hooks
+- tailwind css
+- daisyUI
+- react router V6
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5FnJviWnaL-1hxBX1Bx7T-h5JBLAuQxk",
+  authDomain: "alpha-steelwork.firebaseapp.com",
+  projectId: "alpha-steelwork",
+  storageBucket: "alpha-steelwork.appspot.com",
+  messagingSenderId: "617874098890",
+  appId: "1:617874098890:web:83cc415308c7769f41c233",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
