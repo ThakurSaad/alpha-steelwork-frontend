7b9c43149a17f2f811b8f53ac9d77c0851a38baf
thakurSaad <thakursaad613@gmail.com>
2022-05-24 01:49:40 +0600

    reviews fake data

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -13,7 +13,7 @@ const Review = () => {
       className="py-32"
     >
       <h2 className="text-5xl font-semibold text-center">Customer Feedback</h2>
-      <div className="grid gird-cols-1 justify-items-center gap-8 lg:grid-cols-3 bg-white mx-8 lg:mx-8 rounded-md mt-4 p-4">
+      <div className="grid gird-cols-1 justify-items-center gap-8 lg:grid-cols-3 mx-8 lg:mx-8 rounded-md mt-4 p-4">
         <Customer />
         <Customer />
         <Customer />
