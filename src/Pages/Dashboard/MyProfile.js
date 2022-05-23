39e12e67565f15e15366e03c8485886afe09eeed
thakurSaad <thakursaad613@gmail.com>
2022-05-23 21:21:31 +0600

    user data showed on my profile

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -2,12 +2,29 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useQuery } from "react-query";
 import { toast } from "react-toastify";
 import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 
 const MyProfile = () => {
   const [user] = useAuthState(auth);
   const { email, displayName } = user;
+
+  const {
+    data: currentUser,
+    isLoading,
+    refetch,
+  } = useQuery(["user", email], () =>
+    fetch(`http://localhost:5000/users/${email}`).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+  console.log(currentUser);
+  const { education, address, contact, hobby, faceBook, linkedIn } =
+    currentUser;
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -42,18 +59,51 @@ const MyProfile = () => {
     <section>
       <h2 className="text-3xl font-semibold text-primary my-4">My Profile</h2>
       <div className="profile m-4">
-        <div className="avatar">
-          <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <FontAwesomeIcon
-              className="pt-2 pl-4"
-              icon={faUser}
-              size="4x"
-            ></FontAwesomeIcon>
+        <div className="current-profile mt-6 mb-20">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <div className="avatar">
+                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <FontAwesomeIcon
+                    className="pt-2 pl-4"
+                    icon={faUser}
+                    size="4x"
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </figure>
+            <div className="p-8">
+              <h2 className="card-title text-2xl uppercase">{displayName}</h2>
+              <hr className="border-2 my-6 border-accent" />
+              <p className="my-3">
+                <span className="font-semibold">Email :</span> {email}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">Education :</span>{" "}
+                {education ? education : "Not Provided"}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">Address :</span>{" "}
+                {address ? address : "Not Provided"}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">Contact :</span>{" "}
+                {contact ? contact : "Not Provided"}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">Hobby :</span>{" "}
+                {hobby ? hobby : "Not Provided"}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">FaceBook :</span>{" "}
+                {faceBook ? faceBook : "Not Provided"}
+              </p>
+              <p className="my-3">
+                <span className="font-semibold">LinkedIn :</span>{" "}
+                {linkedIn ? linkedIn : "Not Provided"}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="current-profile my-6">
-          <p>{displayName}</p>
-          <p>{email}</p>
         </div>
         <div className="update-profile">
           <form onSubmit={handleSubmit}>
