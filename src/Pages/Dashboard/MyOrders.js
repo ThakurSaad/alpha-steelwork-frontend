21b6af0dbac937a7f9255180358ca2f3702856c8
thakurSaad <thakursaad613@gmail.com>
2022-05-23 15:35:38 +0600

    my order table

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 const MyOrders = () => {
+  
+
   return (
     <section>
-      <h2 className="text-5xl font-semibold">My Orders</h2>
+      <h2 className="text-2xl font-semibold my-4">My Orders</h2>
+      <div className="my-orders-table">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </section>
   );
 };
