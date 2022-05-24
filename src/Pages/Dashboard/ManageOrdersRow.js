4e119c75abdbe60d01eafcbb0073f1daea982bfa
thakurSaad <thakursaad613@gmail.com>
2022-05-25 02:12:07 +0600

    manage orders row

---
+++ src/Pages/Dashboard/ManageOrdersRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ManageOrdersRow = ({ order }) => {
+  return (
+    <tr className="hover">
+      <th>1</th>
+      <td>Cy Ganderton</td>
+      <td>Quality Control Specialist</td>
+      <td>Blue</td>
+    </tr>
+  );
+};
+
+export default ManageOrdersRow;
