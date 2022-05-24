5c2e859310e7da2258d1613e4772e37eafeadf9e
thakurSaad <thakursaad613@gmail.com>
2022-05-25 01:54:46 +0600

    manage orders table

--- src/Pages/Dashboard/ManageOrders.js
+++ src/Pages/Dashboard/ManageOrders.js
@@ -3,7 +3,29 @@ import React from "react";
 const ManageOrders = () => {
   return (
     <section>
-      <h2 className="text-3xl font-semibold text-primary my-4">Manage All Orders</h2>
+      <h2 className="text-3xl font-semibold text-primary my-4">
+        Manage All Orders
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
