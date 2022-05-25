165e3fc69383a48ba37eee9e1b44888cb88bb116
thakurSaad <thakursaad613@gmail.com>
2022-05-25 19:06:41 +0600

    profile details

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from "./Profile";
+import ProfileDetails from "./ProfileDetails";
 
 const Portfolio = () => {
   return (
     <section className="min-h-screen">
       <h2 className="text-5xl font-bold text-center py-8">Portfolio</h2>
       <Profile />
+      <ProfileDetails />
     </section>
   );
 };
