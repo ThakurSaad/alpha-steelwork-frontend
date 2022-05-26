228a5cb3ce3b0f4639e842553aef17aac888c53a
thakurSaad <thakursaad613@gmail.com>
2022-05-26 17:11:12 +0600

    contact us form done

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -3,45 +3,54 @@ import React from "react";
 const ContactUs = () => {
   return (
     <div>
-      <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <div class="text-center lg:text-left">
-            <h1 class="text-5xl font-bold">Contact Us</h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:max-w-lg lg:ml-20 lg:text-left md:">
+            <h1 className="text-5xl font-bold">Contact Us</h1>
+            <p className="py-6">
+              We will contact you back in 5 hours max. Please check your email
+              in 5 hours. You can also contact our regional distributor
+              directly.
             </p>
           </div>
-          <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div class="card-body">
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Your Name</span>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Name</span>
                 </label>
                 <input
                   type="text"
                   placeholder="name"
-                  class="input input-bordered"
+                  className="input input-bordered"
                 />
               </div>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">The reason you are contacting</span>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">
+                    The reason you are contacting
+                  </span>
                 </label>
                 <textarea
                   type="text"
                   placeholder="Reason"
-                  class="input input-bordered"
+                  className="input input-bordered"
                 />
-                <label class="label">
-                  <a href="#" class="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
               </div>
-              <div class="form-control mt-6">
-                <button class="btn btn-primary">contact us</button>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary text-white">
+                  Send Email
+                </button>
               </div>
             </div>
           </div>
