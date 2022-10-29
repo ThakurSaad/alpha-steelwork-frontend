c26f45dd9ab7d6a101dfc680dc51e7504029bdb0
ThakurSaad <thakursaad613@gmail.com>
2022-10-29 13:27:56 +0600

    toolDetails design change

--- src/Pages/Home/ToolDetails.js
+++ src/Pages/Home/ToolDetails.js
@@ -12,11 +12,14 @@ const ToolDetails = () => {
   let errorElement;
 
   const { data: tool } = useQuery(["tool", purchaseId], () =>
-    fetch(`https://infinite-basin-98544.herokuapp.com/tool/purchase/${purchaseId}`, {
-      headers: {
-        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    }).then((res) => res.json())
+    fetch(
+      `https://infinite-basin-98544.herokuapp.com/tool/purchase/${purchaseId}`,
+      {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    ).then((res) => res.json())
   );
 
   const { displayName, email } = user || "";
@@ -90,115 +93,140 @@ const ToolDetails = () => {
 
   return (
     <section className="min-h-screen">
-      <div className="tool-details">
-        <div className="card w-80 md:w-9/12 bg-base-100 mx-auto">
-          <figure>
-            <img src={image} alt="tool" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title text-5xl">{name}</h2>
-            <p className="text-2xl">{description}</p>
-            <p className="font-semibold">
-              Minimum Order Quantity : {minOrderQuantity}
-            </p>
-            <p className="font-semibold">
-              Available Quantity : {availQuantity}
-            </p>
-            <p className="font-semibold">
-              Price :{" "}
-              <span className="text-4xl text-secondary font-light">
-                {price}
-              </span>
-              $/per piece
-            </p>
-            <h2 className="text-3xl font-semibold text-primary mt-8">
-              Please Fill Up The Form To Complete The Purchase
-            </h2>
-            <form onSubmit={handleSubmit}>
-              <label className="label">
-                <span className="label-text font-semibold pl-3">Name</span>
-              </label>
-              <input
-                type="text"
-                name="name"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-                defaultValue={displayName}
-                disabled
-              />
-              <label className="label">
-                <span className="label-text font-semibold pl-3">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-                defaultValue={email}
-                disabled
-              />
-              <label className="label">
-                <span className="label-text font-semibold pl-3">
-                  Product Name
-                </span>
-              </label>
-              <input
-                type="text"
-                name="productName"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-                defaultValue={name}
-                disabled
-              />
-              <label className="label">
-                <span className="label-text font-semibold pl-3">Address</span>
-              </label>
-              <input
-                type="text"
-                name="address"
-                placeholder="Your Address"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-                required
-              />
-              <label className="label">
-                <span className="label-text font-semibold pl-3">
-                  Contact No
-                </span>
-              </label>
-              <input
-                type="number"
-                name="contact"
-                placeholder="Your Contact No"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-              />
-              <label className="label">
-                <span className="label-text font-semibold pl-3">Quantity</span>
-              </label>
-              <input
-                type="number"
-                name="quantity"
-                placeholder="Order Quantity"
-                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-                defaultValue={minOrderQuantity}
-                required
-                onChange={(e) => {
-                  setQuantity(e.target.value);
-                }}
-              />
-              <label className="label">
-                <span className="label-text-alt text-red-500">
-                  {" "}
-                  {errorElement}
-                </span>
-              </label>
-              <input
-                className="btn btn-primary w-full max-w-xs block my-4"
-                type="submit"
-                value="Order Now"
-                disabled={
-                  quantity < parseInt(minOrderQuantity) ||
-                  quantity > parseInt(availQuantity)
-                }
-              />
-            </form>
+      <div className="tool-details w-80 md:w-9/12 bg-base-100 mx-auto">
+        <div className="md:flex md:items-center mt-4">
+          <div className="card-photo flex items-center justify-center border rounded-md md:w-[450px]">
+            <figure>
+              <img className="rounded-md" src={image} alt="tool" />
+            </figure>
           </div>
+          <div className="md:mx-4">
+            <h2 className="text-xl">{name}</h2>
+            <p className="relative">
+              <span className="absolute text-primary font-bold">$</span>
+              <span className="text-3xl font-bold pl-3">{price}</span>
+              <span className="absolute bottom-0 font-bold">/piece</span>
+            </p>
+            <p className="text-sm my-1">{description}</p>
+
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className="text-sm font-bold">
+                      Minimum Order Quantity
+                    </span>
+                  </td>
+                  <td>
+                    <span className="text-sm">: {minOrderQuantity}/piece</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="text-sm font-bold">
+                      Available Quantity
+                    </span>
+                  </td>
+                  <td>
+                    <span className="text-sm">: {availQuantity}</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            {/* <p className="text-sm">
+              Minimum Order Quantity : {minOrderQuantity}/piece
+            </p>
+            <p className="text-sm">Available Quantity : {availQuantity}</p> */}
+            <h2 className="mt-2 text-sm font-bold">
+              Please Fill Up The Form Below To Complete The Purchase
+            </h2>
+          </div>
+        </div>
+
+        <div className="order-form">
+          <form onSubmit={handleSubmit}>
+            <label className="label">
+              <span className="label-text font-semibold pl-3">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+              defaultValue={displayName}
+              disabled
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-3">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+              defaultValue={email}
+              disabled
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-3">
+                Product Name
+              </span>
+            </label>
+            <input
+              type="text"
+              name="productName"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+              defaultValue={name}
+              disabled
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-3">Address</span>
+            </label>
+            <input
+              type="text"
+              name="address"
+              placeholder="Your Address"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+              required
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-3">Contact No</span>
+            </label>
+            <input
+              type="number"
+              name="contact"
+              placeholder="Your Contact No"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-3">Quantity</span>
+            </label>
+            <input
+              type="number"
+              name="quantity"
+              placeholder="Order Quantity"
+              className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+              defaultValue={minOrderQuantity}
+              required
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {" "}
+                {errorElement}
+              </span>
+            </label>
+            <input
+              className="btn btn-primary w-full max-w-xs block my-4"
+              type="submit"
+              value="Order Now"
+              disabled={
+                quantity < parseInt(minOrderQuantity) ||
+                quantity > parseInt(availQuantity)
+              }
+            />
+          </form>
         </div>
       </div>
     </section>
