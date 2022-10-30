f439044d5cc8d7130b83eacf027960e2469d359d
ThakurSaad <thakursaad613@gmail.com>
2022-10-30 14:04:48 +0600

    add Review design change

--- src/Pages/Dashboard/AddAReview.js
+++ src/Pages/Dashboard/AddAReview.js
@@ -36,46 +36,55 @@ const AddAReview = () => {
 
   return (
     <section>
-      <h2 className="text-3xl font-semibold text-primary my-4">Add A Review</h2>
+      <h2 className="text-3xl font-semibold text-primary my-4 ml-3">
+        Add A Review
+      </h2>
       <div className="add-review-form">
-        <form className="mx-1" onSubmit={handleSubmit}>
-          <label className="label">
-            <span className="label-text font-semibold pl-3">Name</span>
-          </label>
-          <input
-            type="text"
-            name="name"
-            className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-            defaultValue={user?.displayName}
-            disabled
-          />
-          <label className="label">
-            <span className="label-text font-semibold pl-3">Rating</span>
-          </label>
-          <input
-            type="number"
-            name="rating"
-            className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-            placeholder="Between 1 to 5"
-            min="1"
-            max="5"
-            required
-          />
-          <label className="label">
-            <span className="label-text font-semibold pl-3">Description</span>
-          </label>
-          <textarea
-            type="text"
-            name="description"
-            className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
-            placeholder="Short Description"
-            required
-          />
-          <input
-            className="btn btn-primary w-full max-w-xs block my-4 text-white"
-            type="submit"
-            value="post review"
-          />
+        <form
+          onSubmit={handleSubmit}
+          className="grid sm:grid-cols-2 sm:gap-4 lg:gap-0 mx-auto"
+        >
+          <div>
+            <label className="label">
+              <span className="label-text font-semibold pl-2">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="input rounded-md w-full max-w-xs pl-1 ml-2"
+              defaultValue={user?.displayName}
+              disabled
+            />
+            <label className="label">
+              <span className="label-text font-semibold pl-2">Rating</span>
+            </label>
+            <input
+              type="number"
+              name="rating"
+              className="input rounded-md w-full max-w-xs pl-1 ml-2"
+              placeholder="Between 1 to 5"
+              min="1"
+              max="5"
+              required
+            />
+          </div>
+          <div className="relative">
+            <label className="label">
+              <span className="label-text font-semibold pl-2">Description</span>
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              className="input rounded-md w-full max-w-xs pl-1 ml-2"
+              placeholder="Short Description"
+              required
+            />
+            <input
+              className="btn btn-primary absolute sm:bottom-0 text-white w-full rounded-full max-w-xs block mt-4"
+              type="submit"
+              value="post review"
+            />
+          </div>
         </form>
       </div>
     </section>
