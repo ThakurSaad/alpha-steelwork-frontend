4e5c6fe79c897b2bc551e7be0112f39735d90ce2
ThakurSaad <thakursaad613@gmail.com>
2022-10-31 17:00:57 +0600

    footer done

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import footerBg from "../../assets/footer-bg.png";
+import youTube from "../../assets/youTube.png";
+import linkedin from "../../assets/linkedin.png";
+import gitHub from "../../assets/github.png";
+import twitter from "../../assets/twitter.png";
+import facebook from "../../assets/facebook.png";
+import leetCode from "../../assets/leetcode.png";
 
 const Footer = () => {
   return (
@@ -12,7 +18,61 @@ const Footer = () => {
           backgroundSize: "cover",
         }}
       >
-        <footer className="footer p-10">
+        <footer className="footer p-10 justify-items-center md:justify-items-start">
+          <div className="grid grid-cols-3 gap-6">
+            <a
+              href="https://www.youtube.com/channel/UCOmHUn--16B90oW2L6FRR3A"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img src={youTube} alt="social-icon" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/thakur-saad/"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img src={linkedin} alt="social-icon" />
+            </a>
+            <a
+              href="https://github.com/ThakurSaad"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img
+                className="bg-white rounded-full"
+                src={gitHub}
+                alt="social-icon"
+              />
+            </a>
+            <a
+              href="https://twitter.com/Thakur_Saad_B"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img src={twitter} alt="social-icon" />
+            </a>
+            <a
+              href="https://www.facebook.com/Shadow.Monarch.Saad/"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img src={facebook} alt="social-icon" />
+            </a>
+            <a
+              href="https://leetcode.com/thakursaad/"
+              target="_blank"
+              rel="noreferrer"
+              className="link link-hover"
+            >
+              <img src={leetCode} alt="social-icon" />
+            </a>
+          </div>
           <div>
             <span className="footer-title">Services</span>
             <Link to="/" className="link link-hover">
@@ -73,43 +133,6 @@ const Footer = () => {
               AlPHA STEELWORK LTD. <br />
               Providing reliable tech since 1992
             </p>
-          </div>
-          <div className="md:place-self-center md:justify-self-end">
-            <div className="grid grid-flow-col gap-4">
-              <Link to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                </svg>
-              </Link>
-              <Link to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                </svg>
-              </Link>
-              <Link to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                </svg>
-              </Link>
-            </div>
           </div>
         </footer>
       </section>
