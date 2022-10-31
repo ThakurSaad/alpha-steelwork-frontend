7b12eac01f15f2b411442ae0cde81658829ff17b
ThakurSaad <thakursaad613@gmail.com>
2022-10-31 14:33:51 +0600

    benefactor developed

--- src/Pages/Home/Benefactors.js
+++ src/Pages/Home/Benefactors.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+// images
+import thakur from "../../assets/thakur.jpeg";
+import burhan from "../../assets/burhan.jpg";
+import jakir from "../../assets/jakir.jpg";
+import mash from "../../assets/mash.jpg";
+import shujon from "../../assets/shujon.jpg";
+import fonoy from "../../assets/fonoy.jpg";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,77 +20,92 @@ import styles from "./SwiperStyles/Benefactors.module.css";
 
 // import required modules
 import { EffectCube, Pagination } from "swiper";
+import Benefactor from "./Benefactor";
 
 const Benefactors = () => {
-  const benefactor = [
+  const benefactors = [
     {
+      _id: 1,
       name: "Thakur Saad",
-      image: "../../assets/thakur.jpeg",
+      image: thakur,
+      profession: "Software Developer",
       speech:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
     },
     {
+      _id: 2,
       name: "Kazi Samia",
-      image: "../../assets/burhan.jpg",
+      image: burhan,
+      profession: "Investor",
       speech:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
     },
     {
+      _id: 3,
       name: "Zaowad Nasif",
-      image: "../../assets/jakir.jpg",
+      image: jakir,
+      profession: "Marine Investor",
       speech:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
     },
     {
+      _id: 4,
       name: "Jannatul Maisha",
-      image: "../../assets/mash.jpg",
+      image: mash,
+      profession: "Business Analyst",
       speech:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
     },
     {
+      _id: 5,
       name: "Aditto Roy",
-      image: "../../assets/shujon.jpg",
+      image: shujon,
+      profession: "Chef & Investor",
+      speech:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
+    },
+    {
+      _id: 6,
+      name: "Fonoy Acharjee",
+      image: fonoy,
+      profession: "Software Engineer",
       speech:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptatibus a necessitatibus? Hic magnam esse voluptatibus quaerat, repudiandae quam nihil voluptate fugit!",
     },
   ];
 
   return (
-    <section>
+    <>
       <h2 className="text-5xl font-semibold text-center pt-32">
         Our Benefactors
       </h2>
-      <section className="h-[400px] sm:h-[450px]">
-        <div className="relative lg:h-[500px]">
-          <Swiper
-            effect={"cube"}
-            grabCursor={true}
-            cubeEffect={{
-              shadow: true,
-              slideShadows: true,
-              shadowOffset: 20,
-              shadowScale: 0.94,
-            }}
-            pagination={true}
-            modules={[EffectCube, Pagination]}
-            className={`mySwiper ${styles.swiper}`}
-          >
-            <SwiperSlide className={styles.swiperSlide}>
-              <div className="w-[300px] h-[300px] bg-accent rounded-md"></div>
-            </SwiperSlide>
-            <SwiperSlide className={styles.swiperSlide}>
-              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-            </SwiperSlide>
-            <SwiperSlide className={styles.swiperSlide}>
-              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-            </SwiperSlide>
-            <SwiperSlide className={styles.swiperSlide}>
-              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-            </SwiperSlide>
-          </Swiper>
-        </div>
+      <section className="grid md:grid-cols-2 lg:mx-32">
+        <section className="hidden md:block"></section>
+        <section className="h-[400px] sm:h-[450px]">
+          <div className="relative lg:h-[500px]">
+            <Swiper
+              effect={"cube"}
+              grabCursor={true}
+              cubeEffect={{
+                shadow: true,
+                slideShadows: true,
+                shadowOffset: 20,
+                shadowScale: 0.94,
+              }}
+              pagination={true}
+              modules={[EffectCube, Pagination]}
+              className={`mySwiper ${styles.swiper}`}
+            >
+              {benefactors.map((benefactor) => (
+                <SwiperSlide key={benefactor._id}>
+                  <Benefactor benefactor={benefactor} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </section>
       </section>
-    </section>
+    </>
   );
 };
 
